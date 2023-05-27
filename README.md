189ab2d39d23aa9a13491525b7ae70f144f64453
Sean <ussa.say@gmail.com>
2023-05-27 16:23:00 -0700

    Write unit tests

--- README.md
+++ README.md
@@ -9,3 +9,11 @@ Sean Yen
 2. Would you use an end to end test to check if a function is returning the correct output? (yes/no)
 
    **no**
+
+3. Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+
+   **No** because it involves many moving parts, like connecting to a back-end service, a text box allowing the user to enter in their message, a send button, validation to ensure no empty strings or bad words, etc.
+
+4. Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+
+   **Yes** since it is a small-scale encapsulated unit.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,82 @@
 const functions = require('../code-to-unit-test/unit-test-me.js')
 
 // TODO - Part 2
+test("accepts Thomas A. Powell's phone number", () => {
+  expect(functions.isPhoneNumber('858-270-2086')).toBe(true)
+})
+
+test('accepts whatever Google gives for "ucsd phone number"', () => {
+  expect(functions.isPhoneNumber('(858) 534-2230')).toBe(true)
+})
+
+test("rejects Thomas A. Powell's other phone number", () => {
+  expect(functions.isPhoneNumber('888.355.PINT')).toBe(false)
+})
+
+test('rejects foreign phone numbers', () => {
+  expect(functions.isPhoneNumber('+886 970699044')).toBe(false)
+})
+
+test("accepts Thomas A. Powell's email address", () => {
+  expect(functions.isEmail('tpowell2@ucsd.edu')).toBe(true)
+})
+
+test('accepts invalid domain names', () => {
+  expect(functions.isEmail('tpowell2@thomas.pow')).toBe(true)
+})
+
+test('rejects Taipei MRT emails', () => {
+  expect(functions.isEmail('support@metro.taipei')).toBe(false)
+})
+
+test('rejects my email', () => {
+  expect(functions.isEmail('seanthesheep22+lab8@outlook.com')).toBe(false)
+})
+
+test("accepts Thomas A. Powell's password", () => {
+  expect(functions.isStrongPassword('LLMfetish')).toBe(true)
+})
+
+test('accepts "password"', () => {
+  expect(functions.isStrongPassword('password')).toBe(true)
+})
+
+test('rejects XKCD-suggested password', () => {
+  expect(functions.isStrongPassword('correct horse battery staple')).toBe(false)
+})
+
+test('rejects passwords that are too DEI', () => {
+  expect(functions.isStrongPassword('你好１２３')).toBe(false)
+})
+
+test("accepts Thomas A. Powell's birthdate", () => {
+  expect(functions.isDate('8/10/1968')).toBe(true)
+})
+
+test('accepts nonexistent dates', () => {
+  expect(functions.isDate('31/4/2023')).toBe(true)
+})
+
+test('rejects international standards', () => {
+  expect(functions.isDate('1989-06-04')).toBe(false)
+})
+
+test('rejects the future', () => {
+  expect(functions.isDate('1/1/10000')).toBe(false)
+})
+
+test("accepts Thomas A. Powell's skin color", () => {
+  expect(functions.isHexColor('#B86A63')).toBe(true)
+})
+
+test('accepts words', () => {
+  expect(functions.isHexColor('Facade')).toBe(true)
+})
+
+test('rejects translucent colors', () => {
+  expect(functions.isHexColor('#B86A6380')).toBe(false)
+})
+
+test('rejects numbers', () => {
+  expect(functions.isHexColor('0')).toBe(false)
+})
